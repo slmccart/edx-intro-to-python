231b24a09277e994261cdea99988dee58a203f18
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-28 14:52:43 -0400

    Format to 2 digits with format string

--- Functions-and-variables/calculator.py
+++ Functions-and-variables/calculator.py
@@ -2,8 +2,7 @@
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-#Round to 2 digits
-z = round(x / y, 2)
+z = x / y
 
-#Format answer with comma separators
-print(f"{z:,}")
+#Format answer to limit to 2 decimal digits
+print(f"{z:.2f}")
