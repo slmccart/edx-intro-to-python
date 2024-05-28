86c154c2b56a2af7f1e4d55bf67cec97f4279c91
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-28 15:05:28 -0400

    Restructure and introduce main()

--- Functions-and-variables/hello.py
+++ Functions-and-variables/hello.py
@@ -1,6 +1,8 @@
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
 def hello(to="world"):
     print("hello, " + to)
 
-hello()
-name = input("What's your name? ")
-hello(name)
+main()
