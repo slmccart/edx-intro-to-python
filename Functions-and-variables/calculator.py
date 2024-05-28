a03ec5c7dcab6b5013163419fd8c1d80fe5adc57
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-28 14:50:39 -0400

    Round to 2 digits

--- Functions-and-variables/calculator.py
+++ Functions-and-variables/calculator.py
@@ -2,8 +2,8 @@
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-#Round to nearest int
-z = round(x + y)
+#Round to 2 digits
+z = round(x / y, 2)
 
 #Format answer with comma separators
 print(f"{z:,}")
