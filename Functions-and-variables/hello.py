a1fe187e896411ee57781d4847875a952febffa3
Scott.McCarthy <scott.mccarthy@scottmcrthysmbp.myfiosgateway.com>
2024-05-28 12:28:05 -0400

    Split input into first and last name, but only print first

--- Functions-and-variables/hello.py
+++ Functions-and-variables/hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name, remove whitespace, and capitalize
 name = input("What's your name? ").strip().title()
 
+#Split user's name into first name and last name
+first, last = name.split(" ")
+
 # Say hello to user
-print(f"hello, {name}")
+print(f"hello, {first}")
