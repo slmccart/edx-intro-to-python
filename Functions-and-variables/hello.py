497f18f9230e1e1c407e18a358f393421fc9f583
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-28 14:58:41 -0400

    Print from newly defined helper function

--- Functions-and-variables/hello.py
+++ Functions-and-variables/hello.py
@@ -1,8 +1,5 @@
-# Ask user for their name, remove whitespace, and capitalize
-name = input("What's your name? ").strip().title()
+def hello(to):
+    print("hello, " + to)
 
-#Split user's name into first name and last name
-first, last = name.split(" ")
-
-# Say hello to user
-print(f"hello, {first}")
+name = input("What's your name? ")
+hello(name)
