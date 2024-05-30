9f2f03c897f9762ee856ede4ba676c479c349a8a
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-30 15:00:12 -0400

    Grab multiple arguments from sys.argv

--- Libraries/name.py
+++ Libraries/name.py
@@ -3,8 +3,7 @@ import sys
 # Check for errors
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments")
 
 # Print name tags
-print("hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
