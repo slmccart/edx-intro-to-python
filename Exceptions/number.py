0e47a5b27cadaa4355cfb4f7ffeb74bc5cae0ad1
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-30 09:31:17 -0400

    Loop until user enters a valid int

--- Exceptions/number.py
+++ Exceptions/number.py
@@ -1,6 +1,9 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
