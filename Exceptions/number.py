29c86083ca8716e080cd77c7a0e3e51aa0f22db3
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-30 09:36:34 -0400

    Implement with a function

--- Exceptions/number.py
+++ Exceptions/number.py
@@ -1,9 +1,12 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+
+main()
