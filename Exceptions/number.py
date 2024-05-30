335af14808739bf8951c87c8c230ac119d20ef0a
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-30 09:45:31 -0400

    Pass prompt to get_int function

--- Exceptions/number.py
+++ Exceptions/number.py
@@ -1,11 +1,11 @@
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input(prompt))
         except ValueError:
             pass
 
