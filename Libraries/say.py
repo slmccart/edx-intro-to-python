b076bcdda6585b74da1e190223474fb22bb4c9e4
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-05-30 16:01:19 -0400

    Create new sayings module and use it

--- Libraries/say.py
+++ Libraries/say.py
@@ -1,6 +1,5 @@
-import cowsay
 import sys
+from sayings import hello
 
 if len(sys.argv) == 2:
-    cowsay.cow("hello, " + sys.argv[1])
-    cowsay.trex("hello, " + sys.argv[1])
+    hello(sys.argv[1])
