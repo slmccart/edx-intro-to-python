15d7699c997c145043965d88fa6598b5c3ec6581
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-03 18:49:02 -0400

    Implement sorting function with lambda function

--- File-IO/students.py
+++ File-IO/students.py
@@ -6,9 +6,5 @@ with open("students.csv") as file:
         students.append({"name": name, "house": house})
 
 
-def get_name(student):
-    return student["name"]
-
-
-for student in sorted(students, key=get_name):
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
