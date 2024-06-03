0c0bed62b193e62a2a9a54f372af76d8098d4073
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-03 13:55:20 -0400

    Install auto-formatter and test with style problems

---
+++ Style/students.py
@@ -0,0 +1,14 @@
+# Before auto-formatting with 'black'
+# students = {"Hermione": "Gryffindor", "Harry": "Gryffindor", "Ron": "Gryffindor", "Draco": "Slytherin", "Padma": "Ravenclaw"}
+# for student in students:
+#   print(student)
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclaw",
+}
+
+for student in students:
+    print(student)
