73697053e4c400041ee620577ddc1208174ac5a2
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-03 18:17:12 -0400

    Several methods for reading files

--- File-IO/names.py
+++ File-IO/names.py
@@ -1,4 +1,20 @@
-name = input("What's your name? ")
+# Create txt file by writing
+# name = input("What's your name? ")
 
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n")
+
+
+# Read each line in file and process immediately
+# with open("names.txt", "r") as file:
+#     for line in file:
+#         print("hello,", line.rstrip())
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
