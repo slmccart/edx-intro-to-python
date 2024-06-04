4d1af8ab818b88349dbe61942a33c850909fca8c
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 09:36:18 -0400

    Use DictReader to allow flexibility in ordering of columns in csv file

--- File-IO/students.py
+++ File-IO/students.py
@@ -3,9 +3,9 @@ import csv
 students = []
 
 with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
 
 for student in sorted(students, key=lambda student: student["name"]):
