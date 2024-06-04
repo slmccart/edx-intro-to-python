12308bfd6f185cd8b9290a796599a818a48c05d0
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 15:29:21 -0400

    Use non-capturing group to ignore the optional protocol and www

--- Regular-Expressions/twitter.py
+++ Regular-Expressions/twitter.py
@@ -2,5 +2,7 @@ import re
 
 url = input("URL: ").strip()
 
-if matches := re.search(r"^(https?://)?(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
-    print(f"Username: {matches.group(3)}")
+if matches := re.search(
+    r"^(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE
+):
+    print(f"Username: {matches.group(1)}")
