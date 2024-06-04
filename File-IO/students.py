da3d1ce2daec3c238c024ec6fe5d205e08fb9fef
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 09:45:05 -0400

    Write csv file with DictWriter

--- File-IO/students.py
+++ File-IO/students.py
@@ -1,12 +1,21 @@
 import csv
 
-students = []
+# Code for reading csv
+# students = []
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
+# with open("students.csv") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         students.append({"name": row["name"], "home": row["home"]})
 
 
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
+
+# Code for writing csv
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
