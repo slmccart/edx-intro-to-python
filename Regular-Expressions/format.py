b85ff4288c8178dc911d80d630c190197db6c5a3
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 15:00:04 -0400

    Allow arbitrary number of spaces is Last, First format

--- Regular-Expressions/format.py
+++ Regular-Expressions/format.py
@@ -2,7 +2,7 @@ import re
 
 name = input("What's your name? ").strip()
 
-matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), *(.+)$", name)
 if matches:
     last, first = matches.groups()
     # Alternatively last = matches.group(1) and first = matches.group(2)
