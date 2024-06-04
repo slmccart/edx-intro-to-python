338745120b95a4acfe2a175de1d8a02ba249a12c
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 15:02:22 -0400

    Use walrus (:=) operator to condense assignment and conditional

--- Regular-Expressions/format.py
+++ Regular-Expressions/format.py
@@ -2,8 +2,7 @@ import re
 
 name = input("What's your name? ").strip()
 
-matches = re.search(r"^(.+), *(.+)$", name)
-if matches:
+if matches := re.search(r"^(.+), *(.+)$", name):
     last, first = matches.groups()
     # Alternatively last = matches.group(1) and first = matches.group(2)
     name = f"{first} {last}"
