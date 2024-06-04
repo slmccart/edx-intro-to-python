2b6e896e0310c7259eb2d3af8ded8378d66abe72
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 14:43:50 -0400

    Ignore case and allow subdomains

--- Regular-Expressions/validate.py
+++ Regular-Expressions/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w+@\w+\.edu$", email):
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
