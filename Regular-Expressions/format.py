ed4d2046bcf0e2cbe67e34912056ab22d68c676c
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 14:57:23 -0400

    Use regex to capture user input and reformat

---
+++ Regular-Expressions/format.py
@@ -0,0 +1,10 @@
+import re
+
+name = input("What's your name? ").strip()
+
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+    last, first = matches.groups()
+    # Alternatively last = matches.group(1) and first = matches.group(2)
+    name = f"{first} {last}"
+print(f"hello, {name}")
