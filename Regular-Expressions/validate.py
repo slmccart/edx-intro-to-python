104785a7baee0411c94e38b360718bc1ea6f6474
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 14:27:17 -0400

    Restrict email addresses to contain word characters

--- Regular-Expressions/validate.py
+++ Regular-Expressions/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r".+@.+\.edu", email):
+if re.search(r"^\w+@\w+\.edu$", email):
     print("Valid")
 else:
     print("Invalid")
