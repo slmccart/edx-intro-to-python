90362d14b0c8655ec5c86152033d5257432bf03f
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-04 15:27:23 -0400

    Use re.search and capturing groups to capture the username

--- Regular-Expressions/twitter.py
+++ Regular-Expressions/twitter.py
@@ -2,5 +2,5 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
-print(f"Username: {username}")
+if matches := re.search(r"^(https?://)?(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username: {matches.group(3)}")
