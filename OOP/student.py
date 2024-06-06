4f54ec12a6af59c94012549c95e12c245b32e292
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 12:19:47 -0400

    Add the Student.patronus instance variable and use it in a new Student.charm() method

--- OOP/student.py
+++ OOP/student.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
 
@@ -9,20 +9,34 @@ class Student:
 
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🦌"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "🪄"
+
 
 def main():
     student = get_student()
-    print(student)
+    print("Expecto Patronum!")
+    print(student.charm())
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
