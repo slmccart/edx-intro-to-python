ca5e35074133e2901a2d16cdb3394c42d3702e5b
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 12:41:39 -0400

    Define getters and setters for Student.name and Student.house

--- OOP/student.py
+++ OOP/student.py
@@ -1,42 +1,42 @@
 class Student:
-    def __init__(self, name, house, patronus):
-        if not name:
-            raise ValueError("Missing name")
-
-        valid_houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-        if house not in valid_houses:
-            raise ValueError(f"house must be one of: {valid_houses}")
-
+    def __init__(self, name, house):
         self.name = name
         self.house = house
-        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "🦌"
-            case "Otter":
-                return "🦦"
-            case "Jack Russell terrier":
-                return "🐶"
-            case _:
-                return "🪄"
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        valid_houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+        if house not in valid_houses:
+            raise ValueError(f"house must be one of: {valid_houses}")
+        self._house = house
 
 
 def main():
     student = get_student()
-    print("Expecto Patronum!")
-    print(student.charm())
+    print(student)
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)
+    return Student(name, house)
 
 
 if __name__ == "__main__":
