c3938d0cbe5166cf16c6212b4d7fc166bed92076
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 11:58:25 -0400

    Validate input on Student instantiation

--- OOP/student.py
+++ OOP/student.py
@@ -1,5 +1,12 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+
+        valid_houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+        if house not in valid_houses:
+            raise ValueError(f"house must be one of: {valid_houses}")
+
         self.name = name
         self.house = house
 
