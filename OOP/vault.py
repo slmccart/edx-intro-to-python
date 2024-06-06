78069c6e050b9c892ba613570e222665f36e793a
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 17:45:28 -0400

    Overload "+" operator with __add__ method on Vault class

---
+++ OOP/vault.py
@@ -0,0 +1,25 @@
+class Vault:
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+    def __str__(self):
+        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+
+    def __add__(self, other):
+        return Vault(
+            self.galleons + other.galleons,
+            self.sickles + other.sickles,
+            self.knuts + other.knuts,
+        )
+
+
+potter = Vault(100, 50, 25)
+print(potter)
+
+weasley = Vault(25, 50, 100)
+print(weasley)
+
+total = potter + weasley
+print(total)
