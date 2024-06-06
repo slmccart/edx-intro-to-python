a173a641175cc0bd817bb01403d49fa82886ffdf
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 11:50:43 -0400

    Define a Student class and instantiate

--- OOP/student.py
+++ OOP/student.py
@@ -1,13 +1,18 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+
 def main():
     student = get_student()
-    print(f"{student["name"]} from {student["house"]}")
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 
 if __name__ == "__main__":
