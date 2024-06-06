8bc0cff9c1ccce696fe3f964833e2778eb4574ee
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 14:43:53 -0400

    Convert Hat to a static class

--- OOP/hat.py
+++ OOP/hat.py
@@ -2,17 +2,17 @@ import random
 
 
 class Hat:
-    def __init__(self) -> None:
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-    def sort(self, name):
-        house = random.choice(self.houses)
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        house = random.choice(cls.houses)
         print(name, "is in", house)
 
 
 def main():
-    hat = Hat()
-    hat.sort("Harry")
+    Hat.sort("Harry")
 
 
 if __name__ == "__main__":
