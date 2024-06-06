55a569f477cd4702993d6fa8b37510b5282cd1f4
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-06 14:41:48 -0400

    Create Sorting hat class

---
+++ OOP/hat.py
@@ -0,0 +1,19 @@
+import random
+
+
+class Hat:
+    def __init__(self) -> None:
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    def sort(self, name):
+        house = random.choice(self.houses)
+        print(name, "is in", house)
+
+
+def main():
+    hat = Hat()
+    hat.sort("Harry")
+
+
+if __name__ == "__main__":
+    main()
