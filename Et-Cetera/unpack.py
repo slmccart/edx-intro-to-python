6a71f8b7758d3718e0c029481f03dde90178d9b9
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 12:43:52 -0400

    Variable number of positional or named arguments

--- Et-Cetera/unpack.py
+++ Et-Cetera/unpack.py
@@ -1,8 +1,15 @@
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
+def f(*args, **kwargs):
+    print("Named:", kwargs)
+    # print("Positional:", args)
 
 
-coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+f(galleons=100, sickles=50, knuts=25)
 
-print(total(**coins), "Knuts")
-# Unpacks to the equivalent of total(galleons=100, sickles=50, knuts=25)
+# def total(galleons, sickles, knuts):
+#     return (galleons * 17 + sickles) * 29 + knuts
+
+
+# coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+# print(total(**coins), "Knuts")
+# # Unpacks to the equivalent of total(galleons=100, sickles=50, knuts=25)
