adb42733f5ed8b7a63fcf9ea19fe4b6452d3f092
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 10:00:37 -0400

    Add type hints for checking with mypy

--- Et-Cetera/meows.py
+++ Et-Cetera/meows.py
@@ -1,10 +1,7 @@
-class Cat:
-    MEOWS = 3
-
-    def meow(self):
-        for _ in range(Cat.MEOWS):
-            print("meow")
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
 
 
-cat = Cat()
-cat.meow()
+number: int = int(input("Number: "))
+meow(number)
