9c1171f5a0c0729c45462888ecf1bd019cc2832b
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 12:14:21 -0400

    Use argparse library to handle parsing of command-line arguments and generating usage text

--- Et-Cetera/meows.py
+++ Et-Cetera/meows.py
@@ -1,13 +1,11 @@
-import sys
+import argparse
 
-if len(sys.argv) == 1:
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
+args = parser.parse_args()
+
+for _ in range(args.n):
     print("meow")
-elif len(sys.argv) == 3 and sys.argv[1] == "-n":
-    n = int(sys.argv[2])
-    for _ in range(n):
-        print("meow")
-else:
-    print("usage: meows.py -n ")
 
 # def meow(n: int) -> str:
 #     """
