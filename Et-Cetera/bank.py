64a26952cf65e4ec073fe04464aac8886395aa07
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 09:33:43 -0400

    Reimplement bank functionality with a class

--- Et-Cetera/bank.py
+++ Et-Cetera/bank.py
@@ -1,21 +1,24 @@
-balance = 0
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n
+
+    def withdraw(self, n):
+        self._balance -= n
 
 
 def main():
-    print("Balance:", balance)
-    deposit(100)
-    withdraw(50)
-    print("Balance:", balance)
-
-
-def deposit(n):
-    global balance
-    balance += n
-
-
-def withdraw(n):
-    global balance
-    balance -= n
+    account = Account()
+    print("Balance:", account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance:", account.balance)
 
 
 if __name__ == "__main__":
