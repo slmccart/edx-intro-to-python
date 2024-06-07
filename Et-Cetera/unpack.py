6ba19779a983536f0609f77160259dc7ccebaddd
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 12:31:37 -0400

    Unpack dictionary into named parameters with "**" operator

--- Et-Cetera/unpack.py
+++ Et-Cetera/unpack.py
@@ -2,6 +2,7 @@ def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
 
-coins = [100, 50, 25]
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
-print(total(*coins), "Knuts")
+print(total(**coins), "Knuts")
+# Unpacks to the equivalent of total(galleons=100, sickles=50, knuts=25)
