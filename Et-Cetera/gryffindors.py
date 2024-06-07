9fdd9116987104c92e2cb57a853e36c6d8c369e5
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 13:17:42 -0400

    Build dictionary with dictionary comprehension

--- Et-Cetera/gryffindors.py
+++ Et-Cetera/gryffindors.py
@@ -1,6 +1,6 @@
 students = ["Hermione", "Harry", "Ron"]
 
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+gryffindors = {student: "Gryffindor" for student in students}
 
 
 print(gryffindors)
