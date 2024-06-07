adea656b2e0f9f3b2b8efce7648bc6dc3ca692fe
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-07 12:06:43 -0400

    Basic command-line argument parsing with flags

--- Et-Cetera/meows.py
+++ Et-Cetera/meows.py
@@ -1,16 +1,27 @@
-def meow(n: int) -> str:
-    """
-    Meow n times.
+import sys
 
-    :param n: Number of times to meow
-    :type n: int
-    :raise TypeError: If n is not an int
-    :return: A string of n meows, oner per line
-    :rtype: str
-    """
-    return "meow\n" * n
+if len(sys.argv) == 1:
+    print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
+else:
+    print("usage: meows.py -n ")
+
+# def meow(n: int) -> str:
+#     """
+#     Meow n times.
+
+#     :param n: Number of times to meow
+#     :type n: int
+#     :raise TypeError: If n is not an int
+#     :return: A string of n meows, oner per line
+#     :rtype: str
+#     """
+#     return "meow\n" * n
 
 
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+# number: int = int(input("Number: "))
+# meows: str = meow(number)
+# print(meows, end="")
