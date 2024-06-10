771613c894aafae24944e15484bd8be6bfa02ef6
Scott McCarthy <scott.mccarthy@alum.mit.edu>
2024-06-10 14:52:09 -0400

    Shift error checking to encode function so it's more testable

--- Final-Project/project.py
+++ Final-Project/project.py
@@ -3,22 +3,34 @@ from cipher import Cipher
 
 
 def main():
-    phrase = input("What do you want to encode? ").upper()
-    # Check for non-alpha
-    if not phrase.isalpha():
-        sys.exit("Only alphabetic characters are allowed")
-
+    phrase = input("What do you want to encode? ")
     try:
         shift = int(input("Shift value: "))
     except ValueError:
         sys.exit("Shift value must be an integer")
 
-    print(encode(phrase, shift))
+    try:
+        encoded_phrase = encode(phrase, shift)
+    except ValueError:
+        sys.exit("Entered phrase must only contain alpha characters or spaces")
+
+    print(phrase.upper())
+    print(encoded_phrase)
 
 
 def encode(s, shift):
+    encoded_phrase = ""
     cipher = Cipher(shift)
-    return cipher.encode(s)
+
+    words = s.upper().split(" ")
+    for word in words:
+        # Check for non-alpha
+        if not word.isalpha():
+            raise ValueError
+        else:
+            encoded_phrase += cipher.encode(word) + " "
+
+    return encoded_phrase.strip()
 
 
 if __name__ == "__main__":
--- Final-Project/test_project.py
+++ Final-Project/test_project.py
@@ -1,5 +1,15 @@
 from project import encode
+import pytest
 
 
-def test_encode():
+def test_encode_word():
     assert encode("ABC", 2) == "CDE"
+
+
+def test_encode_phrase():
+    assert encode("This is a test", 2) == "VJKU KU C VGUV"
+
+
+def test_encode_nonalpha():
+    with pytest.raises(ValueError):
+        encode("This is CS50", 2)
